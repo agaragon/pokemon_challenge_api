f3edc009e965ced3619f73a4ad6d7d2a7a57b0dc
Andre Guimaraes Aragon <andregaragon@gmail.com>
2022-02-07 00:14:29 -0300

    read me criado

---
+++ README.md
@@ -0,0 +1,39 @@
+# pokemon_api
+
+Essa aplicação tem como objetivo registrar e fornecer os registros dos pokemons capturados pelo usuário.
+
+## Iniciand a aplicação
+
+Para iniciar a aplicação, certifique-se de que está no diretório de nível mais alto do projeto, no mesmo nível do arquivo
+docker-compose.yml. Em seguide rode o comando:
+`sudo docker-compose up`
+Esse comando iniciará a aplicação principal, assim como um banco mongodb e uma rede de conexão entre os dois. A aplicação é
+servida na porta 8080.
+
+A aplicação conta também com um arquivo `.env`, o qual aqui não está no `.gitignore`. Nesse arquivo `.env` estão
+contidas as credenciais do banco de dados. Se houver necessidade de alterá-las, também é necessário alterar as variáveis
+utilizadas na inicializão do banco mongo diretamente no arquivo docker-compose.yml
+
+## Utilizando a aplicação
+
+A aplicação conta com apenas um endpoint, com os métodos Get e Post.
+Endpoint: `http://localhost:8080/pokemon`
+GET: retorna um json com todos os pokemons já capturados pelo usuário
+    Exemplo de payload: [
+    {"nome":"Charizard",
+    "id":180,
+    "imageUrl":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png"},
+    ...
+]
+POST:
+    Recebe o nome de um pokemon e busca em `https://pokeapi.co/` o id do pokemon e a url de sua imagem. Em seguida
+    registra o nome do pokemon, seu id e Url no banco Mongo e fornece uma resposta ao usuário.
+    Exemplo de payload enviado para o endpoint:    {"nome":"charizard"}
+    Resposta:
+    {"nome":"Charizard",
+    "id":180,
+    "imageUrl":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png"
+    }
+    Caso haja algum erro durante a busca das informações do pokemon no banco, o usuário é informado de que o pokemon
+não pode ser encontrado e é levantado um erro de não encontrado.
+    
--- src/main/kotlin/com/aragon/pokemon_api/controllers/PokemonController.kt
+++ src/main/kotlin/com/aragon/pokemon_api/controllers/PokemonController.kt
@@ -5,6 +5,8 @@ import com.aragon.pokemon_api.models.Pokemon
 import com.aragon.pokemon_api.mongoRepositories.PokemonRepository
 import org.json.JSONObject
 import java.net.URI
+import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus
 
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -15,26 +17,41 @@ import java.net.http.HttpResponse
 class PokemonController(
     val pokemonRepository: PokemonRepository
 ) {
+    //É necessário permitir as origens por que o Front End recebe mensagem de CORS caso contrário
     @CrossOrigin(origins = ["*"])
     @GetMapping
-    fun getPokemons(): MutableList<Pokemon> {
-        return pokemonRepository.findAll()
+    fun getPokemons(): ResponseEntity<Any> {
+        // Tenta buscar a lista de pokemons capturados, se não for possível encontrá-la, é por que o banco de dados
+        // está indisponível, nesse caso é enviada uma mensagem de erro para o front junto com código 500
+        return try{
+            ResponseEntity(pokemonRepository.findAll(),HttpStatus.OK)
+        } catch (e:Exception){
+            ResponseEntity("Banco de dados indisponível erro = " + e.message,HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+
     }
+    //É necessário permitir as origens por que o Front End recebe mensagem de CORS caso contrário
     @CrossOrigin(origins = ["*"])
     @PostMapping
     fun savePokemon(@RequestBody pokemon: Pokemon): Any? {
-
-        val client = HttpClient.newBuilder().build()
-        val request = HttpRequest.newBuilder()
-            .uri(URI.create("https://pokeapi.co/api/v2/pokemon/" + pokemon.nome.lowercase()))
-            .build()
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        val gameIndexObj = JSONObject(response.body()).getJSONArray("game_indices")[0]
-        val pokemonIndex =  JSONObject(gameIndexObj.toString())["game_index"] as Int
-        val imageUrl = JSONObject(response.body()).getJSONObject("sprites")["back_default"] as String
-        pokemon.id = pokemonIndex
-        pokemon.imageUrl = imageUrl
-        pokemonRepository.save(pokemon)
-        return pokemon
+        //Tenta buscar as informações do pokemon na "pokeapi". Se encontrar o pokemon, devolve o id do mesmo, juntamente
+        //com o url de sua imagem e o nome dado anteriormente.
+        try{
+            val client = HttpClient.newBuilder().build()
+            val request = HttpRequest.newBuilder()
+                .uri(URI.create("https://pokeapi.co/api/v2/pokemon/" + pokemon.nome.lowercase()))
+                .build()
+            val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            val gameIndexObj = JSONObject(response.body()).getJSONArray("game_indices")[0]
+            val pokemonIndex =  JSONObject(gameIndexObj.toString())["game_index"] as Int
+            val imageUrl = JSONObject(response.body()).getJSONObject("sprites")["back_default"] as String
+            pokemon.id = pokemonIndex
+            pokemon.imageUrl = imageUrl
+            pokemonRepository.save(pokemon)
+            return ResponseEntity(pokemon,HttpStatus.OK)
+        } catch (e:Exception){
+            val errorMessage= "Pokemon não encontrado. Certifíque-se de que escreveu o nome do pokemon corretamente e que sua conexão está funcionando corretamente "
+            return ResponseEntity(errorMessage,HttpStatus.NOT_FOUND)
+        }
     }
 }
