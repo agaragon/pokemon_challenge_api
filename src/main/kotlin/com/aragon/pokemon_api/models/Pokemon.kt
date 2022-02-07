9db5ba6c53bf4c7304714cfa987c8a46ffbf9256
Andre Guimaraes Aragon <andregaragon@gmail.com>
2022-02-07 00:16:58 -0300

    mais documentação adicionada

--- src/main/kotlin/com/aragon/pokemon_api/models/Pokemon.kt
+++ src/main/kotlin/com/aragon/pokemon_api/models/Pokemon.kt
@@ -3,6 +3,7 @@ package com.aragon.pokemon_api.models
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
+// Como o usuário entra apenas com o nome do pokemon, id e imageUrl são facultativos
 @Document
 data class Pokemon(
     var id: Int?,
--- src/main/kotlin/com/aragon/pokemon_api/mongoRepositories/PokemonRepository.kt
+++ src/main/kotlin/com/aragon/pokemon_api/mongoRepositories/PokemonRepository.kt
@@ -3,4 +3,6 @@ package com.aragon.pokemon_api.mongoRepositories
 import org.springframework.data.mongodb.repository.MongoRepository
 import com.aragon.pokemon_api.models.Pokemon
 
+
+//Essa interface permite o registro e recuperação dos pokemons no mongo
 interface PokemonRepository : MongoRepository<Pokemon, Int>
